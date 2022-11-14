8d225958ed588d9a5c3d1461774a51de813e1e0f
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-14 21:53:55 +0600

    Update Zadacha27.cs

--- Zadacha27.cs
+++ Zadacha27.cs
@@ -1,1 +1,10 @@
-
+Console.WriteLine("Введите число :");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Sum = 0;
+while (Number > 0)
+{
+int num = Number % 10;
+Number = Number / 10;
+Sum = Sum + num;
+}
+Console.WriteLine("Cумма цифр в числе равна: " + Sum);
