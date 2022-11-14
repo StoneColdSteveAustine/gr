81679271a96712c0b78585a764bafbbb7be6c5a6
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-14 21:57:32 +0600

    Update Zadacha29.cs

--- Zadacha29.cs
+++ Zadacha29.cs
@@ -1,1 +1,11 @@
-
+Console.WriteLine("Ввод массива");
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+ Console.Write($"Введите элемент массива под индексом {i}:\t");
+ array[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < 8; i++)
+{
+ Console.Write(array[i]);
+}
