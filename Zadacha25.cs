0db6e1e6bbaaeeeb87f9bb7617c1d61f524be2b3
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-14 21:53:20 +0600

    Update Zadacha25.cs

--- Zadacha25.cs
+++ Zadacha25.cs
@@ -1,1 +1,11 @@
+Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = A;
 
+for (int index = 1; index < B; index++)
+{
+C = C * A;
+}
+Console.WriteLine("A в степени B равно: " + C);
